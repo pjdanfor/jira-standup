6cc17d41db4f9fd813530817653c1671451c1408
Kion Kaimi <Kion.Kaimi@allscripts.com>
2023-08-17 09:30:48 -0700

    JS-0002 add support for new enhanced jira board

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -6,11 +6,16 @@ import "./content.css";
 
 (async () => {
     try {
-        const controls = await waitForElement("#ghx-controls", 5000);
+        const isOldJiraVersion = document.body.classList.contains('ghx-agile')
+        const controls = isOldJiraVersion ? await waitForElement("#ghx-controls", 5000) : await waitForElement(`[data-test-id="software-board.header.controls-bar"]`, 5000)
         if (controls) {
             const rootElement = document.createElement("div");
             rootElement.id = "jira-standup";
-            controls.appendChild(rootElement);
+            if (isOldJiraVersion) {
+                controls.appendChild(rootElement);
+            } else {
+                controls.insertAdjacentElement("afterend", rootElement);
+            }
 
             const root = ReactDOM.createRoot(rootElement);
             root.render(
